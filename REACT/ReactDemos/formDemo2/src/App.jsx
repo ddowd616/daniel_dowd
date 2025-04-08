b569f83f622c30612ff5b080eaf9e5d8f74b551e
Daniel Dowd <daniel.h.dowd.mil@army.mil>
2025-04-08 10:35:38 -0500

    got form working with onChange,handleChange

--- REACT/ReactDemos/formDemo2/src/App.jsx
+++ REACT/ReactDemos/formDemo2/src/App.jsx
@@ -13,20 +13,36 @@ const App = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log(formData)
+        //send to database
+        //axios call
     }
 
-    const handleChange = () => {
-
+    const handleChange = (event) => {
+        setFormData(
+            {...formData, //spread operator so we dont lose current values
+                [event.target.name]:event.target.value} //integrate current values into previous values
+        )
+        console.log(formData)
     }
 
     return (
         <>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="" className="fname" >First Name</label>
-                <input onChange={handleChange} type="text" name="fname" id="" value={fname}/>
+                <input
+                    onChange={handleChange}
+                    type="text"
+                    name="fname"
+                    id=""
+                    value={fname}/>
                 <br/>
                 <label htmlFor="" className="age">Age</label>
-                <input onChange={handleChange} type="text" name="age" id="" value={age}/>
+                <input
+                    onChange={handleChange}
+                    type="text"
+                    name="age"
+                    id=""
+                    value={age}/>
                 <br/>
                 <button type="submit">Submit</button>
             </form>
