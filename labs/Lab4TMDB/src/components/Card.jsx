23f44c260546551dec085afc8478b1158f57ac94
Daniel Dowd <daniel.h.dowd.mil@army.mil>
2025-04-17 10:34:08 -0500

    added the scroll ability and fixed width's with the Box sx

--- labs/Lab4TMDB/src/components/Card.jsx
+++ labs/Lab4TMDB/src/components/Card.jsx
@@ -1,34 +1,41 @@
 import React from 'react';
-import { Card as MUICard, CardMedia, CardContent, Typography } from '@mui/material';
+import {Card as MUICard, CardMedia, CardContent, Typography, Grid, Box} from '@mui/material';
+import missingPic from '../assets/confused-what.gif'
 
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 const DEFAULT_IMG = '/shining.jpg';
 
 function Card({ movie }) {
     return (
-        <MUICard sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-            <CardMedia
-                component="img"
-                image={movie.poster_path ? `${IMAGE_BASE_URL}${movie.poster_path}` : DEFAULT_IMG}
-                alt={movie.title}
-                sx={{
-                    height: 350,
-                    objectFit: 'contain',
-                    backgroundColor: '#000', // adds padding area in background if needed
-                }}
-            />
-            <CardContent sx={{ flexGrow: 1 }}>
-                <Typography variant="h6" gutterBottom noWrap>
-                    {movie.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    {movie.overview.length > 100 ? movie.overview.slice(0, 100) + '...' : movie.overview}
-                </Typography>
-                <Typography variant="caption" display="block" mt={1}>
-                    Rating: {movie.vote_average}
-                </Typography>
-            </CardContent>
-        </MUICard>
+        <Grid container direction='row' spacing={2} >
+            <MUICard sx={{ height: '100%', display: 'flex', flexDirection: 'column',marginTop:4 }}>
+                <CardMedia
+                    component="img"
+                    image={movie.poster_path ? `${IMAGE_BASE_URL}${movie.poster_path}` : `${missingPic}` }
+                    alt={movie.title}
+                    sx={{
+                        height: 350,
+                        objectFit: 'contain',
+                        backgroundColor: '#000', // adds padding area in background if needed
+                    }}
+                />
+                <Box sx={{height:120, width: 360, overflowY:'auto',textAlign:'left',paddingRight:1}}>
+                    <CardContent sx={{ flexGrow: 1 }}>
+                        <Typography variant="h6" gutterBottom noWrap>
+                            {movie.title}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            { movie.overview}
+                        </Typography>
+                        <Typography variant="caption" display="block" mt={1}>
+                            Rating: {movie.vote_average}
+                        </Typography>
+                    </CardContent>
+                </Box>
+
+            </MUICard>
+        </Grid>
+
     );
 }
 
